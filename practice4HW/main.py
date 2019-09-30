ce7c8281beed20b9b4c547d249554f6bdbfc2ae1
Denis <dmarkitanov@gmail.com>
2019-09-30 15:13:56 +0600

    done

--- practice4HW/main.py
+++ practice4HW/main.py
@@ -14,47 +14,55 @@ def hypothesis(X, thetas):
 def gradient(thetas, X, Y):
     return hypothesis(X, thetas) - Y
 
-def recieve_data():
-    with open('dataset.csv') as csv_file:
+def recieve_data(file):
+    with open(file) as csv_file:
         rawData = list(csv.reader(csv_file, delimiter=','))
         data = np.delete(rawData, 0, axis=0)
 
         return data
 
-data = recieve_data()
+data = recieve_data('dataset.csv')
 
 Y = [data[i][3] for i in range(len(data))]
 Y = np.asarray(Y, dtype=np.float)
 X = np.delete(data, 3, axis=1)
 X = np.asarray(X, dtype=np.float)
 
-# Y = [(i-np.mean(Y))/(np.max(Y)-np.min(Y)) for i in Y]
 X = np.array([[(X[i][j]-np.mean(X[:, j]))/(np.max(X[:, j])-np.min(X[:, j])) for j in range(len(X[i]))] for i in range(len(X))])
 
 thetas = np.zeros(4, dtype=np.float32)
  
-ax = pl.figure().gca(projection="3d")
-ax.scatter(X[0], X[1], X[2], Y)
-pl.show()
+# ax = pl.figure().gca(projection="3d")
+# ax.scatter(X[0], X[1], X[2], Y)
+# pl.show()
 
 X = np.array([np.insert(i, 0, 1) for i in X])
-
-print(X.shape)
-print(thetas.shape)
 
 alpha = 0.01
 epochs = 1500
 
 for i in range(epochs):
     next_thetas = np.zeros(4).astype(float)
-    for j in range(3):
+    for j in range(4):
         next_thetas[j] = thetas[j] - (alpha / len(X)) * np.sum((hypothesis(X, thetas) - Y) * np.array(X[:, j]))
     thetas = next_thetas
 
 print(thetas)
 # print('-----')
 # # print(hypothesis(X, thetas))
-print(np.dot(X, thetas))
+probs = np.dot(X, thetas)
+print(probs)
+print(np.sum(probs) / len(probs))
+
+train_data_raw = recieve_data('train.csv')
+
+train_x = np.asarray(train_data_raw, dtype=np.float)
+
+train_x = np.array([[(train_x[i][j]-np.mean(train_x[:, j]))/(np.max(train_x[:, j])-np.min(train_x[:, j])) for j in range(len(train_x[i]))] for i in range(len(train_x))])
+
+train_x = np.array([np.insert(i, 0, 1) for i in train_x])
+
+print(np.dot(train_x, thetas))
 
 # # point = np.array([0.0, 0.0, thetas[0]])
 # # normal = np.array(cross([1, 0, thetas[2]], [0, 1, thetas[1]]))
