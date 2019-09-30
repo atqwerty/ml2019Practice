7f9a926d5718fe7c2cdb4f231ac9d31c11166972
Denis <dmarkitanov@gmail.com>
2019-09-30 10:26:47 +0600

    convertion of data is done

--- practice4HW/main.py
+++ practice4HW/main.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as pl
 from mpl_toolkits.mplot3d import Axes3D
+import csv
 
 def cross(a, b):
     return [a[1] * b[2] - a[2] * b[1],
@@ -13,41 +14,55 @@ def hypothesis(X, thetas):
 def gradient(thetas, X, Y):
     return hypothesis(X, thetas) - Y
 
+def recieve_data():
+    with open('dataset.csv') as csv_file:
+        rawData = list(csv.reader(csv_file, delimiter=','))
+        data = np.delete(rawData, 0, axis=0)
 
-dataRaw = np.loadtxt(fname="ex2data1.txt", delimiter=",", usecols=[0, 1, 2])
-Y = [dataRaw[i][2] for i in range(len(dataRaw))]
+        return data
+
+data = recieve_data()
+
+Y = [data[i][3] for i in range(len(data))]
 Y = np.asarray(Y, dtype=np.float)
-X = np.delete(dataRaw, 2, 1)
-# X = X.transpose()
+X = np.delete(data, 3, axis=1)
+X = np.asarray(X, dtype=np.float)
 
-thetas = np.zeros(3, dtype=np.float32)
 
-ax = pl.figure().gca(projection="3d")
+# dataRaw = np.loadtxt(fname="ex2data1.txt", delimiter=",", usecols=[0, 1, 2])
+# Y = [dataRaw[i][2] for i in range(len(dataRaw))]
+# Y = np.asarray(Y, dtype=np.float)
+# X = np.delete(dataRaw, 2, 1)
+# # X = X.transpose()
 
-# ax.scatter(X[0], X[1], Y)
+# thetas = np.zeros(3, dtype=np.float32)
 
-# X = X.transpose()
-X = np.array([np.insert(i, 0, 1) for i in X])
+# ax = pl.figure().gca(projection="3d")
 
-alpha = 0.01
-epochs = 1500
+# # ax.scatter(X[0], X[1], Y)
 
-for i in range(epochs):
-    next_thetas = np.zeros(3).astype(float)
-    for j in range(3):
-        next_thetas[j] = thetas[j] - (alpha / len(X)) * np.sum((hypothesis(X, thetas) - Y) * np.array(X[:, j]))
-    thetas = next_thetas
+# # X = X.transpose()
+# X = np.array([np.insert(i, 0, 1) for i in X])
 
-print(thetas)
-print('-----')
-# print(hypothesis(X, thetas))
-propbs = np.dot(X, thetas)
-print(np.sum(propbs) / len(propbs))
+# alpha = 0.01
+# epochs = 1500
 
-# point = np.array([0.0, 0.0, thetas[0]])
-# normal = np.array(cross([1, 0, thetas[2]], [0, 1, thetas[1]]))
-# xx, yy = np.meshgrid([0, 1], [0, 1])
-# z = (-normal[0] * xx - normal[1] * yy + point.dot(normal)) * 1. / normal[2]
-# ax.plot_surface(xx, yy, z, alpha=0.2, color="red")
+# for i in range(epochs):
+#     next_thetas = np.zeros(3).astype(float)
+#     for j in range(3):
+#         next_thetas[j] = thetas[j] - (alpha / len(X)) * np.sum((hypothesis(X, thetas) - Y) * np.array(X[:, j]))
+#     thetas = next_thetas
 
-# pl.show()
+# print(thetas)
+# print('-----')
+# # print(hypothesis(X, thetas))
+# propbs = np.dot(X, thetas)
+# print(np.sum(propbs) / len(propbs))
+
+# # point = np.array([0.0, 0.0, thetas[0]])
+# # normal = np.array(cross([1, 0, thetas[2]], [0, 1, thetas[1]]))
+# # xx, yy = np.meshgrid([0, 1], [0, 1])
+# # z = (-normal[0] * xx - normal[1] * yy + point.dot(normal)) * 1. / normal[2]
+# # ax.plot_surface(xx, yy, z, alpha=0.2, color="red")
+
+# # pl.show()
