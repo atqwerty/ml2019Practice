fd327d68d68e8e1f4ef523717f5e1d13f9955f75
Denis <dmarkitanov@gmail.com>
2019-09-30 15:26:20 +0600

    final commit

--- practice4HW/main.py
+++ practice4HW/main.py
@@ -31,10 +31,6 @@ X = np.asarray(X, dtype=np.float)
 X = np.array([[(X[i][j]-np.mean(X[:, j]))/(np.max(X[:, j])-np.min(X[:, j])) for j in range(len(X[i]))] for i in range(len(X))])
 
 thetas = np.zeros(4, dtype=np.float32)
- 
-# ax = pl.figure().gca(projection="3d")
-# ax.scatter(X[0], X[1], X[2], Y)
-# pl.show()
 
 X = np.array([np.insert(i, 0, 1) for i in X])
 
@@ -47,27 +43,31 @@ for i in range(epochs):
         next_thetas[j] = thetas[j] - (alpha / len(X)) * np.sum((hypothesis(X, thetas) - Y) * np.array(X[:, j]))
     thetas = next_thetas
 
-print(thetas)
-# print('-----')
-# # print(hypothesis(X, thetas))
+print('Thetas: ' + str(thetas))
+
 probs = np.dot(X, thetas)
-print(probs)
-print(np.sum(probs) / len(probs))
+
+trashhold = np.sum(probs) / len(probs)
+print('Trash hold: ' + str(trashhold))
 
 train_data_raw = recieve_data('train.csv')
 
 train_x = np.asarray(train_data_raw, dtype=np.float)
-
 train_x = np.array([[(train_x[i][j]-np.mean(train_x[:, j]))/(np.max(train_x[:, j])-np.min(train_x[:, j])) for j in range(len(train_x[i]))] for i in range(len(train_x))])
-
 train_x = np.array([np.insert(i, 0, 1) for i in train_x])
 
-print(np.dot(train_x, thetas))
+print('-----')
+predictions = np.dot(train_x, thetas)
+print('Predictions: ' + str(predictions))
 
-# # point = np.array([0.0, 0.0, thetas[0]])
-# # normal = np.array(cross([1, 0, thetas[2]], [0, 1, thetas[1]]))
-# # xx, yy = np.meshgrid([0, 1], [0, 1])
-# # z = (-normal[0] * xx - normal[1] * yy + point.dot(normal)) * 1. / normal[2]
-# # ax.plot_surface(xx, yy, z, alpha=0.2, color="red")
+answer = np.zeros(5, dtype=str)
 
-# # pl.show()
+counter = 0
+
+for value in predictions:
+    if (value >= trashhold):
+        answer[counter] = 'yes'
+    else: answer[counter] = 'no'
+    counter += 1
+
+print(answer)
