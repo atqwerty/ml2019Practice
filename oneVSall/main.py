5e4323ae43a4e18c8aca98317a46f3c7c64e458d
Denis <dmarkitanov@gmail.com>
2019-10-09 21:19:33 +0600

    onevsall

--- oneVSall/main.py
+++ oneVSall/main.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as pl
 import csv
+import random
 
 def sigmoid(z):
     return 1 / (1 + np.exp(-z))
@@ -19,10 +20,11 @@ Y = [data[i][9] for i in range(len(data))]
 X = np.delete(data, 9, axis=1)
 X = np.asarray(X, dtype=np.float)
 
-X = np.array([[(X[i][j]-np.mean(X[:, j]))/(np.max(X[:, j])-np.min(X[:, j])) for j in range(len(X[i]))] for i in range(len(X))])
+# X = np.array([[(X[i][j]-np.mean(X[:, j]))/(np.max(X[:, j])-np.min(X[:, j])) for j in range(len(X[i]))] for i in range(len(X))])
 
 X = np.insert(X, 0, 1, axis=1)
-thetas = np.zeros(X.shape[1], dtype=np.float32)
+# thetas = np.zeros(X.shape[1], dtype=np.float32)
+thetas = np.random.rand(X.shape[1])
 
 classifiers = np.zeros(shape=(6, 10))
 
@@ -38,6 +40,16 @@ for i in np.unique(Y):
         output = X.dot(thetas)
 
         errors = sigmoid(output) - y_copy
-        thetas -= alpha / len(X) * errors.dot(X)
+        thetas -= (alpha / len(X)) * errors.dot(X)
     answer.append((thetas, i))
-print(answer)
+# print(answer)
+
+train_x = X[:10]
+
+for i in range(6):
+    print(answer[i][0])
+
+# for i in range(train_x.shape[0]):
+    # for j in range(6):
+        # print(answer[j][0])
+        # print(np.dot(train_x[i], answer[j][0]))
--- practice4/main.py
+++ practice4/main.py
@@ -10,11 +10,17 @@ def mapFeature(X1, X2):
             out = np.hstack((out, np.multiply(np.power(X1, i-j), np.power(X2, j))[:,np.newaxis]))
     return out
 
+def cross(a, b):
+    return [a[1] * b[2] - a[2] * b[1],
+            a[2] * b[0] - a[0] * b[2],
+            a[0] * b[1] - a[1] * b[0]]
+
 def hypothesis(X, thetas):
     return 1 / (1 + np.exp(-X.dot(thetas)))
 
 def gradient(thetas, X, Y):
     return hypothesis(X, thetas) - Y
+
 
 dataRaw = np.loadtxt(fname="ex2data2.txt", delimiter=",", usecols=[0, 1, 2])
 Y = [dataRaw[i][2] for i in range(len(dataRaw))]
@@ -23,6 +29,13 @@ X = np.delete(dataRaw, 2, 1)
 
 thetas = np.zeros(28, dtype=np.float32)
 
+# X = X.transpose()
+# pl.scatter(X[0], X[1])
+# X = X.transpose()
+
+# ax = pl.figure().gca(projection="3d")
+# X = X.transpose()
+
 X = np.array([np.insert(i, 0, 1) for i in X])
 X = mapFeature(X[:,1], X[:,2])
 
@@ -30,11 +43,45 @@ alpha = 0.1
 epochs = 100000
 
 for i in range(epochs):
-    difference = hypothesis(X, thetas) - Y
-    thetas -= (alpha / len(X)) * X.T.dot(difference)
+    next_thetas = np.zeros(28).astype(float)
+    for j in range(28):
+        if (j == 0):
+            next_thetas[j] = thetas[j] - (alpha / len(X)) * np.sum((hypothesis(X, thetas) - Y) * np.array(X[:, j]))
+        else:
+            next_thetas[j] = thetas[j] - (alpha / len(X)) * np.sum((hypothesis(X, thetas) - Y) * np.array(X[:, j])) + (0.1 * thetas[j] ** 2)
+    thetas = next_thetas
  
 print('Thetas: ' + str(thetas))
 print('-----')
 
 pred = [hypothesis(X, thetas) >= 0.5]
 print('Accruracy is: ' + str(np.mean(pred == Y.flatten()) * 100) + '%')
+
+u = np.linspace(-1, 1.5, 50)
+v = np.linspace(-1, 1.5, 50)
+z = np.zeros((len(u), len(v)))
+
+# X = X.transpose()
+
+# def mapFeatureForPlotting(X1, X2):
+#     degree = 6
+#     out = np.ones(1)
+#     for i in range(1, degree+1):
+#         for j in range(i+1):
+#             out = np.hstack((out, np.multiply(np.power(X1, i-j), np.power(X2, j))))
+#     return out
+
+# for i in range(len(u)):
+#     for j in range(len(v)):
+#         z[i,j] = np.dot(mapFeatureForPlotting(u[i], v[j]), thetas)
+
+# mask = Y.flatten() == 1
+# # X = data.iloc[:,:-1]
+# passed = pl.scatter(X[mask][0], X[mask][1])
+# failed = pl.scatter(X[~mask][0], X[~mask][1])
+# pl.contour(u,v,z,0)
+# pl.xlabel('Microchip Test1')
+# pl.ylabel('Microchip Test2')
+# pl.legend((passed, failed), ('Passed', 'Failed'))
+
+# pl.show()
