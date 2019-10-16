b047d7d78f6b4660eb35de392080d068267d1002
Denis <dmarkitanov@gmail.com>
2019-10-16 16:44:36 +0600

    added shuffle and normalization

--- oneVSall/main.py
+++ oneVSall/main.py
@@ -14,13 +14,14 @@ def recieve_data(file):
         return data
 
 data = recieve_data('glass.csv')
+np.random.shuffle(data)
 
 Y = [data[i][9] for i in range(len(data))]
 
 X = np.delete(data, 9, axis=1)
 X = np.asarray(X, dtype=np.float)
 
-# X = np.array([[(X[i][j]-np.mean(X[:, j]))/(np.max(X[:, j])-np.min(X[:, j])) for j in range(len(X[i]))] for i in range(len(X))])
+X = np.array([[(X[i][j]-np.mean(X[:, j]))/(np.max(X[:, j])-np.min(X[:, j])) for j in range(len(X[i]))] for i in range(len(X))])
 
 X = np.insert(X, 0, 1, axis=1)
 # thetas = np.zeros(X.shape[1], dtype=np.float32)
@@ -44,14 +45,24 @@ for i in np.unique(Y):
         errors = sigmoid(output) - y_copy
         thetas -= (alpha / len(X)) * errors.dot(X)
     answer.append((thetas, i))
-print(answer)
+# print(answer)
 
 train_x = X[:10]
 
 # for i in range(6):
-    # print(answer[i][0])
+#     print(answer[i][0])
+
+# print(train_x[0])
+# print(answer[0][0])
+
+# print(sigmoid(np.dot(train_x[0], answer[0][0])))
 
 # for i in range(train_x.shape[0]):
-    # for j in range(6):
+    # print(i)
+    # print(np.dot(train_x[0], answer[i][0]))
+    # print('-----')
+
+for i in range(train_x.shape[0]):
+    for j in range(6):
         # print(answer[j][0])
-        # print(np.dot(train_x[i], answer[j][0]))
+        print(sigmoid(np.dot(train_x[i], answer[j][0])))
