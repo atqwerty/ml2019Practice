330b51457b41d2d8c3db151bf19110a6f735e3be
Denis <dmarkitanov@gmail.com>
2019-10-22 13:32:20 +0600

    almost done one vs all

--- oneVSall/main.py
+++ oneVSall/main.py
@@ -21,48 +21,45 @@ Y = [data[i][9] for i in range(len(data))]
 X = np.delete(data, 9, axis=1)
 X = np.asarray(X, dtype=np.float)
 
+x = X
+
 X = np.array([[(X[i][j]-np.mean(X[:, j]))/(np.max(X[:, j])-np.min(X[:, j])) for j in range(len(X[i]))] for i in range(len(X))])
 
-X = np.insert(X, 0, 1, axis=1)
-# thetas = np.zeros(X.shape[1], dtype=np.float32)
-thetas = np.random.rand(X.shape[1])
-
-classifiers = np.zeros(shape=(6, 10))
+# X = np.insert(X, 0, 1, axis=1)
+thetas = np.random.rand(6, X.shape[1])
+print(np.unique(Y))
 
 alpha = 0.01
 epochs = 1500
 
 answer = []
+counter = 0
 
 for i in np.unique(Y):
+    # thetas = np.random.rand(6, X.shape[1])
     y_copy = []
     for j in range(len(Y)):
         y_copy.append(1 if Y[j] == i else 0)
 
-    for _ in range(epochs):
-        output = X.dot(thetas)
+    for j in range(len(X)):
+        output = X.dot(thetas[counter])
 
         errors = sigmoid(output) - y_copy
-        thetas -= (alpha / len(X)) * errors.dot(X)
-    answer.append((thetas, i))
-# print(answer)
+        thetas[counter] -= (alpha / len(X)) * errors.dot(X)
+    # print(thetas[counter])
+    answer.append((thetas[counter], i))
+    counter += 1
 
-train_x = X[:10]
+# print(X.shape)
 
-# for i in range(6):
-#     print(answer[i][0])
+print(answer)
 
-# print(train_x[0])
-# print(answer[0][0])
-
-# print(sigmoid(np.dot(train_x[0], answer[0][0])))
+# train_x = X[:10]
 
 # for i in range(train_x.shape[0]):
-    # print(i)
-    # print(np.dot(train_x[0], answer[i][0]))
-    # print('-----')
+#     for j in range(6):
+#         print(np.sum(sigmoid(np.dot(train_x[i], answer[j][0].T))) / len(sigmoid(np.dot(train_x[i], answer[j][0].T))))
+#         print('-----')
+#     print('---------------------------------------')
 
-for i in range(train_x.shape[0]):
-    for j in range(6):
-        # print(answer[j][0])
-        print(sigmoid(np.dot(train_x[i], answer[j][0])))
+# print(x[-1])
