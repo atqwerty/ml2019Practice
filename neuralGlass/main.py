463a1352501c2e638d8b1b99c6251be816736d54
Denis <dmarkitanov@gmail.com>
2019-10-29 23:33:07 +0600

    refactor is needed

--- neuralGlass/main.py
+++ neuralGlass/main.py
@@ -1,56 +1,105 @@
-import nn
-import pandas
 import numpy as np
+import matplotlib.pyplot as plt
+import pandas
 
+np.random.seed(42)
 data = pandas.read_csv('glass.csv')
 
 data = data.sample(frac=1).reset_index(drop=True)
 
-Y = data['Type']
+labels = data['Type']
 data = data.drop('Type', axis = 1)
 
 data = data.to_numpy()
-Y = Y.to_numpy()
-y = []
+labels = labels.to_numpy()
 
-for i in range(len(Y)):
-    holder = np.zeros(6)
-    if Y[i] == 1:
-        holder[0] = 1
-        
-    if Y[i] == 2:
-        holder[1] = 1
-        
-    if Y[i] == 3:
-        holder[2] = 1
-        
-    if Y[i] == 5:
-        holder[3] = 1
-        
-    if Y[i] == 6:
-        holder[4] = 1
-        
-    if Y[i] == 7:
-        holder[5] = 1
-        
-    y.append(holder)
+one_hot_labels = np.zeros((214, 9))
 
-y = np.asarray(y)
+for i in range(214):
+    one_hot_labels[i, labels[i]] = 1
 
+# np.random.shuffle(data)
 
 X = data
 
-# x = X
+x = X
 
 X = np.array([[(X[i][j]-np.mean(X[:, j]))/(np.max(X[:, j])-np.min(X[:, j])) for j in range(len(X[i]))] for i in range(len(X))])
 
 # X = np.array([np.insert(i, 0, 1) for i in X])
 
-model = nn.NeuralNetwork()
-model.init(X, y)
 
-for i in range(1500):
-    model.feed_forward()
-    model.back_propogation()
 
-print(model.output)
+# plt.figure(figsize=(10,7))
+# plt.scatter(X[:,0], X[:,1], c=labels, cmap='plasma', s=100, alpha=0.5)
+# plt.show()
+
+def sigmoid(x):
+    return 1/(1+np.exp(-x))
+
+def sigmoid_der(x):
+    return sigmoid(x) *(1-sigmoid (x))
+
+def softmax(A):
+    expA = np.exp(A)
+    return expA / expA.sum(axis=1, keepdims=True)
+
+instances = X.shape[0]
+attributes = X.shape[1]
+hidden_nodes = 9
+output_labels = 9
+
+wh = np.random.rand(attributes,hidden_nodes)
+bh = np.random.randn(hidden_nodes)
+
+wo = np.random.rand(hidden_nodes,output_labels)
+bo = np.random.randn(output_labels)
+lr = 0.01
+
+error_cost = []
+
+for epoch in range(15000):
+############# feedforward
+
+    # Phase 1
+    zh = np.dot(X, wh) + bh
+    ah = sigmoid(zh)
+
+    # Phase 2
+    zo = np.dot(ah, wo) + bo
+    ao = softmax(zo)
+
+########## Back Propagation
+
+########## Phase 1
+
+    dcost_dzo = ao - one_hot_labels
+    dzo_dwo = ah
+
+    dcost_wo = np.dot(dzo_dwo.T, dcost_dzo)
+
+    dcost_bo = dcost_dzo
+
+########## Phases 2
+
+    dzo_dah = wo
+    dcost_dah = np.dot(dcost_dzo , dzo_dah.T)
+    dah_dzh = sigmoid_der(zh)
+    dzh_dwh = X
+    dcost_wh = np.dot(dzh_dwh.T, dah_dzh * dcost_dah)
+
+    dcost_bh = dcost_dah * dah_dzh
+
+    # Update Weights ================
+
+    wh -= lr * dcost_wh
+    bh -= lr * dcost_bh.sum(axis=0)
+
+    wo -= lr * dcost_wo
+    bo -= lr * dcost_bo.sum(axis=0)
+
+    if epoch % 200 == 0:
+        loss = np.sum(-one_hot_labels * np.log(ao))
+        print('Loss function value: ', loss)
+        error_cost.append(loss)
+    # print("a")
--- neuralGlass/nn.py
+++ neuralGlass/nn.py
@@ -2,11 +2,12 @@ import numpy as np
 
 class NeuralNetwork:
     def init(self, x, y):
+        np.random.seed(1)
         self.input = x
         self.weights1 = np.random.rand(self.input.shape[1], 9)
-        self.weights2 = np.random.rand(9, 1)
+        self.weights2 = np.random.rand(9, 6)
         self.y = y
-        self.output = np.zeros(self.y.shape)
+        self.output = np.zeros_like(y)
 
     def sigmoid(self, z):
         return 1 / (1 + np.exp(-z))
@@ -16,8 +17,13 @@ class NeuralNetwork:
 
     def feed_forward(self):
         self.layer1 = self.sigmoid(np.dot(self.input, self.weights1))
+        print(self.layer1.shape)
+        print(self.weights2.shape)
         self.output = self.sigmoid(np.dot(self.layer1, self.weights2))
 
     def back_propogation(self):
         d_weights2 = np.dot(self.layer1.T, (2 * (self.y - self.output) * self.sigmoid_derivative(self.output)))
-        d_weights1 = np.dot(self.input.T,  (np.dot(2*(self.y - self.output) * self.sigmoid_derivative(self.output), self.weights2.T) * self.sigmoid_derivative(self.layer1)))
+        d_weights1 = np.dot(self.input.T,  (np.dot((self.y - self.output) * self.sigmoid_derivative(self.output), self.weights2.T) * self.sigmoid_derivative(self.layer1)))
+
+        self.weights1 += d_weights1
+        self.weights2 += d_weights2
