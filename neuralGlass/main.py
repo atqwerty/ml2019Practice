c697c7439788d24b470dbec8b8727c9b60a59833
Denis <dmarkitanov@gmail.com>
2019-11-06 16:10:38 +0600

    now

--- neuralGlass/main.py
+++ neuralGlass/main.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas
 import nn
+import nn2
 
 data = pandas.read_csv('glass.csv')
 
@@ -13,15 +14,16 @@ data = data.drop('Type', axis = 1)
 data = data.to_numpy()
 labels = labels.to_numpy()
 
-one_hot_labels = np.zeros((214, 9))
+one_hot_labels = np.zeros((214, 6))
+print(labels)
 
 for i in range(214):
-    one_hot_labels[i, labels[i]] = 1
+    one_hot_labels[i, labels[i] - 1] = 1
 
 X = data
 
 X = np.array([[(X[i][j]-np.mean(X[:, j]))/(np.max(X[:, j])-np.min(X[:, j])) for j in range(len(X[i]))] for i in range(len(X))])
 
 model = nn.NeuralNetwork()
-model.init(X, one_hot_labels, 15000)
+model.init(X, one_hot_labels, 50000)
 model.train()
